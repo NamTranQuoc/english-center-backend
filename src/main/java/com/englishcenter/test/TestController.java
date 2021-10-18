9b81de41483fb7b811eed765d0d979e2c2592f7e
NamTran <nam.tran@zamo.io>
2021-10-18 20:43:16 +0700

    a

--- src/main/java/com/englishcenter/test/TestController.java
+++ src/main/java/com/englishcenter/test/TestController.java
@@ -1,2 +1,12 @@
-package com.englishcenter.test;public class TestController {
+package com.englishcenter.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/abc")
+    public String get() {
+        return "abc";
+    }
 }
