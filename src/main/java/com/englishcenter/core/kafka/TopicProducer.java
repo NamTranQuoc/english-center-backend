4808cc981309c6320ae3c5c263227fd8426109ad
NamTran <nam.tran@zamo.io>
2022-01-03 20:36:48 +0700

    kafka

--- src/main/java/com/englishcenter/core/kafka/TopicProducer.java
+++ src/main/java/com/englishcenter/core/kafka/TopicProducer.java
@@ -8,10 +8,28 @@ import org.springframework.kafka.config.TopicBuilder;
 @Configuration
 public class TopicProducer {
     public static final String SEND_MAIL = "SEND_MAIL";
+    public static final String UPDATE_SCORE_BY_EXCEL = "UPDATE_SCORE_BY_EXCEL";
+    public static final String GENERATE_SCHEDULE = "GENERATE_SCHEDULE";
 
     @Bean
     public NewTopic sendMail() {
         return TopicBuilder.name(SEND_MAIL)
+                .partitions(10)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    public NewTopic updateScore() {
+        return TopicBuilder.name(UPDATE_SCORE_BY_EXCEL)
+                .partitions(10)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    public NewTopic generateSchedule() {
+        return TopicBuilder.name(GENERATE_SCHEDULE)
                 .partitions(10)
                 .replicas(1)
                 .build();
