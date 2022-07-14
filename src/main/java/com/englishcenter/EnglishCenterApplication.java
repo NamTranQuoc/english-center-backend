b94393ce49fcdaf00380a5380ec6331d456366ee
QuocNam <namtranquoc322@gmail.com>
2022-07-14 09:35:21 +0700

    fix bugs send mail don't work

--- src/main/java/com/englishcenter/EnglishCenterApplication.java
+++ src/main/java/com/englishcenter/EnglishCenterApplication.java
@@ -1,13 +1,9 @@
 package com.englishcenter;
 
-import com.englishcenter.member.application.MemberApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
-
-import java.util.Map;
 
 @SpringBootApplication
 @EnableScheduling
@@ -17,9 +13,9 @@ public class EnglishCenterApplication {
         SpringApplication.run(EnglishCenterApplication.class, args);
     }
 
-    @Scheduled(fixedRate = 60000)
-    private void start0hEveryday() {
-        MemberApplication memberApplication = new MemberApplication();
-        memberApplication.test();
-    }
+//    @Scheduled(fixedRate = 60000)
+//    private void start0hEveryday() {
+//        MemberApplication memberApplication = new MemberApplication();
+//        memberApplication.test();
+//    }
 }
